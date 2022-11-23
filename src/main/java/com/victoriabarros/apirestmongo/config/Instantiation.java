513c4a788f7cf933cea44fff620e724898b078df
Victoria Barros <lis.victoriab@gmail.com>
2022-11-23 14:56:23 -0300

    projeção dos dados do autor com DTO

--- src/main/java/com/victoriabarros/apirestmongo/config/Instantiation.java
+++ src/main/java/com/victoriabarros/apirestmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.victoriabarros.apirestmongo.domain.Post;
 import com.victoriabarros.apirestmongo.domain.User;
+import com.victoriabarros.apirestmongo.dto.AuthorDTO;
 import com.victoriabarros.apirestmongo.repository.PostRepository;
 import com.victoriabarros.apirestmongo.repository.UserRepository;
 
@@ -36,10 +37,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Silva", "alex@gmail.com");
 		User joão = new User(null, "João Santos", "joao@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("30/10/2022"), "bora votar", "já votei, agora é só aguardar", maria);
-		Post post2 = new Post(null, sdf.parse("24/11/2022"), "copa do mundo!", "vai Brasil!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, joão));
+		
+		Post post1 = new Post(null, sdf.parse("30/10/2022"), "bora votar", "já votei, agora é só aguardar", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("24/11/2022"), "copa do mundo!", "vai Brasil!", new AuthorDTO(maria));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
