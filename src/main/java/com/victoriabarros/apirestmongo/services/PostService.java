568729ee0153adafed07b4c20bfb81139bf2558d
Victoria Barros <lis.victoriab@gmail.com>
2022-11-23 15:53:29 -0300

    obter post por id

---
+++ src/main/java/com/victoriabarros/apirestmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.victoriabarros.apirestmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victoriabarros.apirestmongo.domain.Post;
+import com.victoriabarros.apirestmongo.repository.PostRepository;
+import com.victoriabarros.apirestmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
