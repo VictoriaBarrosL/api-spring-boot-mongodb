ec1c6efdf009f6ea4fd939622464431177c2c31a
Victoria Barros <lis.victoriab@gmail.com>
2022-11-23 14:33:02 -0300

    entity POST e user aninhado

--- src/main/java/com/victoriabarros/apirestmongo/config/Instantiation.java
+++ src/main/java/com/victoriabarros/apirestmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.victoriabarros.apirestmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.victoriabarros.apirestmongo.domain.Post;
 import com.victoriabarros.apirestmongo.domain.User;
+import com.victoriabarros.apirestmongo.repository.PostRepository;
 import com.victoriabarros.apirestmongo.repository.UserRepository;
 
 @Configuration
@@ -14,16 +18,29 @@ public class Instantiation implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	
 	@Override
 	public void run(String... args) throws Exception {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GTM"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Souza", "maria@gmail.com");
 		User alex = new User(null, "Alex Silva", "alex@gmail.com");
-		User joão = new User(null, "João Santos", "bob@gmail.com");
+		User joão = new User(null, "João Santos", "joao@gmail.com");
+		
+		Post post1 = new Post(null, sdf.parse("30/10/2022"), "bora votar", "já votei, agora é só aguardar", maria);
+		Post post2 = new Post(null, sdf.parse("24/11/2022"), "copa do mundo!", "vai Brasil!", maria);
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, joão));
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
