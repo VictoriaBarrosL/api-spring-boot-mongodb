14841366f058167ad9b4f8fbb5a6418ea89bf2f8
Victoria Barros <lis.victoriab@gmail.com>
2022-11-30 15:58:54 -0300

    consulta simples com @Query

--- src/main/java/com/victoriabarros/apirestmongo/repository/PostRepository.java
+++ src/main/java/com/victoriabarros/apirestmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.victoriabarros.apirestmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.victoriabarros.apirestmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
--- src/main/java/com/victoriabarros/apirestmongo/services/UserService.java
+++ src/main/java/com/victoriabarros/apirestmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.victoriabarros.apirestmongo.domain.Post;
 import com.victoriabarros.apirestmongo.domain.User;
 import com.victoriabarros.apirestmongo.dto.UserDTO;
 import com.victoriabarros.apirestmongo.repository.UserRepository;
